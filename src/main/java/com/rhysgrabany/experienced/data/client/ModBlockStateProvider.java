ebdbbc5afd9938ea1e889bcac285158cdf3756af
Rhys Grabany <r.grabany@gmail.com>
2021-02-19 15:06:20 +0000

    Adding BlockStateProvider

--- src/main/java/com/rhysgrabany/experienced/data/client/ModBlockStateProvider.java
+++ src/main/java/com/rhysgrabany/experienced/data/client/ModBlockStateProvider.java
@@ -1,4 +1,29 @@
 package com.rhysgrabany.experienced.data.client;
 
-public class ModBlockStateProvider {
+import com.rhysgrabany.experienced.ModBlocks;
+import com.rhysgrabany.experienced.block.ExperienceBlock;
+import com.rhysgrabany.experienced.config.Constants;
+import net.minecraft.data.DataGenerator;
+import net.minecraftforge.client.model.generators.BlockStateProvider;
+import net.minecraftforge.common.data.ExistingFileHelper;
+
+public class ModBlockStateProvider extends BlockStateProvider {
+
+
+    public ModBlockStateProvider(DataGenerator gen, ExistingFileHelper exFileHelper) {
+        super(gen, Constants.MOD_ID, exFileHelper);
+    }
+
+    @Override
+    protected void registerStatesAndModels() {
+
+        simpleBlock(ModBlocks.EXPERIENCE_BLOCKS.get(ExperienceBlock.Tier.SMALL).get());
+        simpleBlock(ModBlocks.EXPERIENCE_BLOCKS.get(ExperienceBlock.Tier.MEDIUM).get());
+        simpleBlock(ModBlocks.EXPERIENCE_BLOCKS.get(ExperienceBlock.Tier.LARGE).get());
+        simpleBlock(ModBlocks.EXPERIENCE_BLOCKS.get(ExperienceBlock.Tier.LARGE).get());
+
+    }
+
+
+
 }
