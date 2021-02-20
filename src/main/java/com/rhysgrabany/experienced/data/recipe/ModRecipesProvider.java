de5f6f5e0863d19b1396f8e8c3ded3c44e05c9f8
Rhys Grabany <r.grabany@gmail.com>
2021-02-20 16:54:52 +0000

    Added ExperienceBlockCasing recipe

--- src/main/java/com/rhysgrabany/experienced/data/recipe/ModRecipesProvider.java
+++ src/main/java/com/rhysgrabany/experienced/data/recipe/ModRecipesProvider.java
@@ -1,10 +1,8 @@
 package com.rhysgrabany.experienced.data.recipe;
 
 import com.rhysgrabany.experienced.ModItems;
-import net.minecraft.data.DataGenerator;
-import net.minecraft.data.IFinishedRecipe;
-import net.minecraft.data.RecipeProvider;
-import net.minecraft.data.ShapelessRecipeBuilder;
+import net.minecraft.data.*;
+import net.minecraft.item.Item;
 import net.minecraft.item.Items;
 
 import java.util.function.Consumer;
@@ -18,11 +16,40 @@ public class ModRecipesProvider extends RecipeProvider {
 
     @Override
     protected void registerRecipes(Consumer<IFinishedRecipe> consumer) {
+        registerItemRecipes(consumer);
+        //registerBlockRecipes(consumer);
+    }
+
+    private void registerItemRecipes(Consumer<IFinishedRecipe> consumer){
+        // Recipe for the Experience Book Item
         ShapelessRecipeBuilder.shapelessRecipe(ModItems.EXPERIENCE_BOOK.get(), 1)
                 .addIngredient(Items.BOOK)
                 .addIngredient(Items.EMERALD)
                 .addIngredient(Items.ENDER_EYE)
                 .addCriterion("has_item", hasItem(Items.EMERALD))
                 .build(consumer);
+
+        //Recipe for the Experience Block Casing Item
+        ShapedRecipeBuilder.shapedRecipe(ModItems.EXPERIENCE_BLOCK_CASING.get(), 3)
+                .key('i', Items.IRON_INGOT)
+                .key('g', Items.GLASS)
+                .key('b', ModItems.EXPERIENCE_BOOK.get())
+                .patternLine("igi")
+                .patternLine("gbg")
+                .patternLine("igi")
+                .addCriterion("has_item", hasItem(ModItems.EXPERIENCE_BOOK.get()))
+                .build(consumer);
+
+
     }
+
+
+    private void registerBlockRecipes(Consumer<IFinishedRecipe> consumer){
+
+    }
+
+
+
+
+
 }
