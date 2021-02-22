a510b46149e935e8c8483847247dd75aad8dc6d3
Rhys Grabany <r.grabany@gmail.com>
2021-02-22 19:27:12 +0000

    Added textures for hovering/selecting of buttons

--- src/main/java/com/rhysgrabany/experienced/gui/SingleMinusButton.java
+++ src/main/java/com/rhysgrabany/experienced/gui/SingleMinusButton.java
@@ -5,10 +5,12 @@ import com.mojang.blaze3d.platform.GlStateManager;
 import com.mojang.blaze3d.systems.RenderSystem;
 import com.rhysgrabany.experienced.config.Constants;
 import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.RenderComponentsUtil;
 import net.minecraft.client.gui.widget.button.Button;
 import net.minecraft.util.ResourceLocation;
 import net.minecraft.util.text.ITextComponent;
 import net.minecraftforge.common.MinecraftForge;
+import org.lwjgl.opengl.GL11;
 
 public class SingleMinusButton extends Button {
 
@@ -30,6 +32,8 @@ public class SingleMinusButton extends Button {
 
         Minecraft minecraft = Minecraft.getInstance();
 
+        isHovered = inBounds(x, y, width, height, mouseX, mouseY);
+
         minecraft.getTextureManager().bindTexture(TEXTURE);
         RenderSystem.enableDepthTest();
         RenderSystem.color4f(1.0f, 1.0f,1.0f,1.0f);
@@ -45,8 +49,20 @@ public class SingleMinusButton extends Button {
 
         this.blit(matrixStack, textureX, textureY, SINGLE_MINUS_BUTTON_XPOS, SINGLE_MINUS_BUTTON_YPOS, width, height);
 
+
+        if(isHovered){
+            RenderSystem.enableBlend();
+            RenderSystem.blendFunc(GL11.GL_SRC_ALPHA, GL11.GL_ONE_MINUS_SRC_ALPHA);
+            RenderSystem.color4f(1.0f, 1.0f, 1.0f, 0.5f);
+            this.blit(matrixStack, );
+        }
+
         this.renderBg(matrixStack, minecraft, mouseX, mouseY);
 
+    }
+
+    private static boolean inBounds(int x, int y, int w, int h, double ox, double oy){
+        return ox >= x && ox <= x + w && oy >= y && oy <= y + h;
     }
 
 
