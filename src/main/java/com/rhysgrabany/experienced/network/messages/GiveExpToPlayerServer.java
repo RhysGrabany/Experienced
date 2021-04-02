c3d8589e09cced185ab7c543ab578ca6085ef3ed
Rhys Grabany <r.grabany@gmail.com>
2021-04-02 14:34:06 +0100

    Making some headway

--- src/main/java/com/rhysgrabany/experienced/network/messages/GiveExpToPlayerServer.java
+++ src/main/java/com/rhysgrabany/experienced/network/messages/GiveExpToPlayerServer.java
@@ -1,17 +1,28 @@
 package com.rhysgrabany.experienced.network.messages;
 
+import net.minecraft.network.PacketBuffer;
+import net.minecraftforge.fml.network.NetworkEvent;
+
+import java.util.function.Supplier;
+
 public class GiveExpToPlayerServer {
+    private int expAmount;
+
+    public GiveExpToPlayerServer(int expAmount){
+        this.expAmount = expAmount;
+    }
 
 
-    public static void encocde(){
+    public static void encocde(GiveExpToPlayerServer msg, PacketBuffer buff){
+        buff.writeInt(msg.expAmount);
 
     }
 
-    public static GiveExpToPlayerServer decode(){
-
+    public static GiveExpToPlayerServer decode(PacketBuffer buff){
+        return new GiveExpToPlayerServer(buff.readInt());
     }
 
-    public static void handle(){
+    public static void handle(GiveExpToPlayerServer msg, Supplier<NetworkEvent.Context> ctxSupplier){
 
     }
 
