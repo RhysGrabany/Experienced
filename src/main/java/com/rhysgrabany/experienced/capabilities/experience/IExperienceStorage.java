d3b808d6a2769eb03fd828cd7e6a6a90a2930fb3
Rhys Grabany <r.grabany@gmail.com>
2021-04-05 12:54:37 +0100

    Adding info to IExperienceStorage

--- src/main/java/com/rhysgrabany/experienced/capabilities/experience/IExperienceStorage.java
+++ src/main/java/com/rhysgrabany/experienced/capabilities/experience/IExperienceStorage.java
@@ -1,4 +1,30 @@
 package com.rhysgrabany.experienced.capabilities.experience;
 
-public class IExperienceStorage {
+
+// Modeled after EnergyStorage from Forge cause that's basically what I want for experience
+public interface IExperienceStorage {
+
+
+    // Adds experience to the storage. Returns quantity of experience that was accepted
+    int receiveExperience(int maxReceive, boolean simulate);
+
+    // Removes experience from the storage. Returns quantity of experience that was removed
+    int extractExperience(int maxExtract, boolean simulate);
+
+    // Returns the amount of experience currently stored
+    int getExperienceStored();
+
+    // Returns the max amount of experience that can be stored
+    int getMaxExperienceStored();
+
+    // Returns if this storage can have experience extracted
+    // If this is false, then any calls to extractExperience will return 0
+    boolean canExtract();
+
+    // Used to determine if this storage can receive experience
+    // If false, then any calls to receiveExperience will return 0
+    boolean canReceive();
+
+
+
 }
